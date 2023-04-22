ebddb22747ce21c0db8d13f46dea1f03632e4af9
SushantHolikar <lcs2020006@iiitl.ac.in>
2023-04-22 15:29:03 +0530

    updated

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ app.use(cors());
 
 const bodyParser = require ('express').json ;
 app.use(bodyParser());
-app.use(cors({
-  origin: 'https://helpinghands-miniproject.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 app.use('/user', UserRouter);
 app.use('/donor', DonorRouter)
 app.use(function(req, res, next) {
@@ -45,6 +39,31 @@ app.use(function(req, res, next) {
     res.status(200).json("File has been uploaded")
   })
   
+  app.post("/payment", cors(), async (req, res) => {
+    let { amount, id } = req.body
+    try {
+      const payment = await stripe.paymentIntents.create({
+        amount,
+        currency: "₹",
+        description: "Donation",
+        payment_method: id,
+        confirm: true
+      })
+      console.log("Payment", payment)
+      res.json({
+        message: "Payment successful",
+        success: true
+      })
+    } catch (error) {
+      console.log("Error", error)
+      res.json({
+        message: "Payment failed",
+        success: false
+      })
+    }
+  })
+  
+
 app.use("/posts", authPost)
 app.use("/events", authEvent)
 app.use("/donationcards", authDonationCard)
